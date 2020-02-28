0916c7824d13f34b53a755ee47a393f6aa3a7f17
forhad-h <hosainf1@gmail.com>
2020-02-29 00:11:04 +0600

    add return id into form as an attribute

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -14,23 +14,26 @@
 
     e.preventDefault()
 
-    var year = $(this).find('input[name=year]').val()
-    var make = $(this).find('input[name=make]').val()
-    var model = $(this).find('input[name=model]').val()
-    var product_url = $(this).find('input[name=product_url]').val()
+    var _this = $(this)
+    var year = $(this).find('input[name=year]')
+    var make = $(this).find('input[name=make]')
+    var model = $(this).find('input[name=model]')
+    var product_url = $(this).find('input[name=product_url]')
 
 
     $.ajax({
       method: 'POST',
       url: apqAjax.url + "?action=save_query_info",
       data: {
-        year: year,
-        make: make,
-        model: model,
-        product_url: product_url
+        year: year.val(),
+        make: make.val(),
+        model: model.val(),
+        product_url: product_url.val()
       }
     }).done(function(data) {
-      console.log("Data", data)
+      var dataObj = JSON.parse(data)[0]
+      _this.attr('data-rid', dataObj.id)
+
     })
 
   })
